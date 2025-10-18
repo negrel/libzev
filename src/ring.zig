e16f6cccce3c7e9c77f9e7c4e6d34e3d9dd64c5b
Alexandre Negrel <alexandre@negrel.dev>
2025-10-18 17:55:01 +0200

    use AND mask similar to io_uring mask instead of slow modulo

--- src/ring.zig
+++ src/ring.zig
@@ -1,57 +1,55 @@
 /// Buffer returns a ring buffer for type T backed by fixed size array.
 pub fn Buffer(T: type, comptime cap: usize) type {
+    if (cap % 2 != 0) @compileError("capacity must be a power of two");
+
     return struct {
         const Self = @This();
 
         pub const capacity = cap;
+        // (x & mask) is used as a fast (x % capacity).
+        pub const mask = capacity - 1;
 
         arr: [capacity]T = undefined,
-        r: usize = 0,
-        w: usize = 0,
+        head: usize = 0,
+        tail: usize = 0,
 
         /// Returns whether ring buffer is full (i.e it contains capacity - 1
         /// items).
-        fn isFull(self: *Self) bool {
-            return self.r == (self.w + 1) % capacity;
+        pub fn isFull(self: *Self) bool {
+            return self.tail - self.head == capacity;
         }
 
         /// Returns whether ring buffer is empty.
-        fn isEmpty(self: *Self) bool {
-            return self.r == self.w;
+        pub fn isEmpty(self: *Self) bool {
+            return self.head == self.tail;
         }
 
         /// Reserve a slot, returns a pointer to it and move write cursor.
-        fn push(self: *Self) ?*T {
-            const nw = (self.w + 1) % capacity;
-            if (self.r == nw) return null; // full.
-
-            const ptr = &self.arr[self.w];
-            self.w = nw;
+        pub fn push(self: *Self) ?*T {
+            if (self.len() >= capacity) return null; // full.
+            const ptr = &self.arr[self.tail & mask];
+            self.tail += 1;
             return ptr;
         }
 
         /// Peeks an entry to be read without consuming it. You must call
         /// `skip()` to move read cursor once you're done with T.
-        fn peek(self: *Self) ?*T {
+        pub fn peek(self: *Self) ?*T {
             if (self.isEmpty()) return null;
-            return &self.arr[self.r];
+            return &self.arr[self.head & mask];
         }
 
         /// Skips at most `n` entries to be read and returns actual number of
         /// skipped entries.
-        fn skip(self: *Self, n: usize) usize {
+        pub fn skip(self: *Self, n: usize) usize {
             const skipped = @min(self.len(), n);
-            self.r += skipped;
+            self.head = (self.head + skipped);
             return skipped;
         }
 
         /// Returns number of entries to be read.
-        fn len(self: *Self) usize {
-            if (self.r > self.w) {
-                return capacity - self.r + self.w;
-            } else {
-                return self.w - self.r;
-            }
+        pub fn len(self: *Self) usize {
+            return self.tail - self.head;
         }
     };
 }
@@ -76,20 +74,27 @@ test {
     try std.testing.expect(rbuf.peek() == null);
     try std.testing.expect(rbuf.len() == 0);
 
-    for (0..1023) |i| {
+    for (0..1024) |i| {
         rbuf.push().?.* = i;
     }
     try std.testing.expect(rbuf.push() == null);
     try std.testing.expect(!rbuf.isEmpty());
     try std.testing.expect(rbuf.isFull());
+    try std.testing.expect(rbuf.len() == 1024);
 
-    try std.testing.expect(rbuf.r > rbuf.w);
-    try std.testing.expect(rbuf.len() == 1023);
-
-    for (0..1023) |i| {
+    for (0..1024) |i| {
         try std.testing.expect(rbuf.peek().?.* == i);
         try std.testing.expect(rbuf.skip(1) == 1);
     }
 
+    try std.testing.expect(rbuf.len() == 0);
+
+    for (0..1024) |i| {
+        rbuf.push().?.* = i;
+    }
+    for (0..1024) |i| {
+        try std.testing.expect(rbuf.peek().?.* == i);
+        try std.testing.expect(rbuf.skip(1) == 1);
+    }
     try std.testing.expect(rbuf.len() == 0);
 }
