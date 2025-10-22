b316925dae47d0467ec1c9ba2d1601b3aec42526
Alexandre Negrel <alexandre@negrel.dev>
2025-10-22 22:06:47 +0200

    add multi thread test for MPSC queue

--- src/queue_mpsc.zig
+++ src/queue_mpsc.zig
@@ -6,6 +6,9 @@ const assert = std.debug.assert;
 /// An intrusive MPSC (multi-provider, single consumer) queue implementation.
 /// The type T must have a field "next" of type `?*T`.
 ///
+/// Once initialized the queue can't be moved, its reference must remain stable
+/// and you should never make copy of it.
+///
 /// This is an implementatin of a Vyukov Queue[1].
 /// TODO(mitchellh): I haven't audited yet if I got all the atomic operations
 /// correct. I was short term more focused on getting something that seemed
@@ -79,7 +82,7 @@ pub fn Intrusive(comptime T: type) type {
     };
 }
 
-test Intrusive {
+test "single thread" {
     const testing = std.testing;
 
     // Types
@@ -115,4 +118,53 @@ test Intrusive {
     q.push(&elems[1]);
     try testing.expect(q.pop().? == &elems[1]);
     try testing.expect(q.pop() == null);
+}
+
+test "multi thread" {
+    const threads_count = 4;
+    const iter_count = 2000;
+
+    const Elem = struct {
+        const Self = @This();
+        next: ?*Self = null,
+    };
+    const Queue = Intrusive(Elem);
+    var q: Queue = undefined;
+    q.init();
+
+    var elems: [threads_count * iter_count]Elem = undefined;
+    var threads: [threads_count]std.Thread = undefined;
+    var pushed: std.atomic.Value(u32) = .init(0);
+
+    for (0..threads_count) |i| {
+        threads[i] = try std.Thread.spawn(.{}, struct {
+            fn worker(queue: *Queue, els: []Elem, count: *std.atomic.Value(u32)) void {
+                var qq = queue;
+                for (0..els.len) |j| {
+                    qq.push(&els[j]);
+                }
+
+                _ = count.fetchAdd(@intCast(els.len), .seq_cst);
+            }
+        }.worker, .{ &q, elems[i * iter_count .. (i + 1) * iter_count], &pushed });
+    }
+
+    var start = try std.time.Timer.start();
+    var popped: usize = 0;
+    while (popped < threads_count * iter_count and start.read() <= std.time.ns_per_s) {
+        const count = pushed.load(.seq_cst) - popped;
+
+        if (count == 0) {
+            std.Thread.yield() catch {};
+            continue;
+        }
+
+        for (0..count) |_| {
+            try std.testing.expect(q.pop() != null);
+        }
+
+        popped += count;
+    }
+
+    try std.testing.expect(popped == threads_count * iter_count);
 }
