f036ffc36e9aec758873990c694f6c9efc411e2c
Alexandre Negrel <alexandre@negrel.dev>
2025-11-01 12:33:36 +0100

    move tests in their own module

--- build.zig
+++ build.zig
@@ -11,13 +11,20 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
 
-    const mod_tests = b.addTest(.{
-        .root_module = mod,
+    const tests_mod = b.addModule("tests", .{
+        .root_source_file = b.path("src/tests.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    tests_mod.addImport("zev", mod);
+
+    const tests = b.addTest(.{
+        .root_module = tests_mod,
         .use_llvm = llvm,
     });
 
-    const run_mod_tests = b.addRunArtifact(mod_tests);
-    const install_mod_tests = b.addInstallArtifact(mod_tests, .{});
+    const run_mod_tests = b.addRunArtifact(tests);
+    const install_mod_tests = b.addInstallArtifact(tests, .{});
 
     const test_step = b.step("test", "Run tests");
     test_step.dependOn(&run_mod_tests.step);
